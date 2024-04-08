b7dd838a639c06405de92ea6892bf1b992c8d7f1
Oleg Nemna <nemnao@gmail.com>
2024-04-08 14:48:52 +0200

    fix

--- hemlibra.js
+++ hemlibra.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const expandButtons = document.querySelectorAll('.expandable-button');
+    const expandVideoLinks = document.querySelectorAll('.expandable-link');
 
     expandButtons.forEach(button => {
         const expandableContent = button.querySelector('.expandable');
@@ -9,12 +10,43 @@ document.addEventListener('DOMContentLoaded', function() {
             if (expandableContent.classList.contains('expanded')) {
                 expandableContent.classList.remove('expanded');
                 expandableContent.style.maxHeight = '0';
-                arrowIcon.style.transform = 'rotate(0)';
+                if (arrowIcon) {
+                    arrowIcon.style.transform = 'rotate(0)';
+                }
             } else {
                 expandableContent.classList.add('expanded');
                 expandableContent.style.maxHeight = expandableContent.scrollHeight + 'px';
-                arrowIcon.style.transform = 'rotate(180deg)';
+                if (arrowIcon) {
+                    arrowIcon.style.transform = 'rotate(180deg)';
+                }
             }
         });
     });
+
+
+    document.querySelectorAll('.expandable-link').forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            const videoId = button.getAttribute('data-video-id');
+            const expandableContent = document.querySelector(`.expandable[data-video-id="${videoId}"]`);
+
+            if (!expandableContent.classList.contains('expanded')) {
+                document.querySelectorAll('.expandable.expanded').forEach(openItem => {
+                    if (openItem.getAttribute('data-video-id') !== videoId) {
+                        openItem.classList.remove('expanded');
+                        openItem.style.maxHeight = '0';
+                    }
+                });
+
+                expandableContent.classList.add('expanded');
+                expandableContent.style.maxHeight = expandableContent.scrollHeight + 'px';
+            } else {
+                expandableContent.classList.remove('expanded');
+                expandableContent.style.maxHeight = '0';
+            }
+        });
+    });
+
+
 });
